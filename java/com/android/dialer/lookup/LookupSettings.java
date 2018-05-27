LineageOS/android_packages_apps_Dialer

Dialer: comply with EU's GDPR
Disable lookup by default and add a disclaimer for the feature

Change-Id: If7a181952304dbaee736762bdfd5819eddc5f89b
Signed-off-by: Joey <joey@lineageos.org>

java/com/android/dialer/lookup/LookupSettings.java
@@ -53,17 +53,17 @@ public final class LookupSettings {
 
     public static boolean isForwardLookupEnabled(Context context) {
         return LineageSettings.System.getInt(context.getContentResolver(),
-                LineageSettings.System.ENABLE_FORWARD_LOOKUP, 1) != 0;
+                LineageSettings.System.ENABLE_FORWARD_LOOKUP, 0) != 0;
     }
 
     public static boolean isPeopleLookupEnabled(Context context) {
         return LineageSettings.System.getInt(context.getContentResolver(),
-                LineageSettings.System.ENABLE_PEOPLE_LOOKUP, 1) != 0;
+                LineageSettings.System.ENABLE_PEOPLE_LOOKUP, 0) != 0;
     }
 
     public static boolean isReverseLookupEnabled(Context context) {
         return LineageSettings.System.getInt(context.getContentResolver(),
-                LineageSettings.System.ENABLE_REVERSE_LOOKUP, 1) != 0;
+                LineageSettings.System.ENABLE_REVERSE_LOOKUP, 0) != 0;
     }
 
     public static String getForwardLookupProvider(Context context) {

java/com/android/dialer/lookup/LookupSettingsFragment.java
@@ -107,11 +107,11 @@ public class LookupSettingsFragment extends PreferenceFragment
     private void restoreLookupProviderSwitches() {
         final ContentResolver cr = getActivity().getContentResolver();
         mEnableForwardLookup.setChecked(LineageSettings.System.getInt(cr,
-                LineageSettings.System.ENABLE_FORWARD_LOOKUP, 1) != 0);
+                LineageSettings.System.ENABLE_FORWARD_LOOKUP, 0) != 0);
         mEnablePeopleLookup.setChecked(LineageSettings.System.getInt(cr,
-                LineageSettings.System.ENABLE_PEOPLE_LOOKUP, 1) != 0);
+                LineageSettings.System.ENABLE_PEOPLE_LOOKUP, 0) != 0);
         mEnableReverseLookup.setChecked(LineageSettings.System.getInt(cr,
-                LineageSettings.System.ENABLE_REVERSE_LOOKUP, 1) != 0);
+                LineageSettings.System.ENABLE_REVERSE_LOOKUP, 0) != 0);
     }
 
     private void restoreLookupProviders() {

lite-os/packages_apps_Dialer

java/com/android/dialer/lookup/LookupSettings.java
@@ -55,17 +55,17 @@ public final class LookupSettings {
 
     public static boolean isForwardLookupEnabled(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
-                Settings.System.ENABLE_FORWARD_LOOKUP, 1) != 0;
+                Settings.System.ENABLE_FORWARD_LOOKUP, 0) != 0;
     }
 
     public static boolean isPeopleLookupEnabled(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
-                Settings.System.ENABLE_PEOPLE_LOOKUP, 1) != 0;
+                Settings.System.ENABLE_PEOPLE_LOOKUP, 0) != 0;
     }
 
     public static boolean isReverseLookupEnabled(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
-                Settings.System.ENABLE_REVERSE_LOOKUP, 1) != 0;
+                Settings.System.ENABLE_REVERSE_LOOKUP, 0) != 0;
     }
 
     public static String getForwardLookupProvider(Context context) {

java/com/android/dialer/lookup/LookupSettingsFragment.java
@@ -107,11 +107,11 @@ public class LookupSettingsFragment extends PreferenceFragment
     private void restoreLookupProviderSwitches() {
         final ContentResolver cr = getActivity().getContentResolver();
         mEnableForwardLookup.setChecked(Settings.System.getInt(cr,
-                Settings.System.ENABLE_FORWARD_LOOKUP, 1) != 0);
+                Settings.System.ENABLE_FORWARD_LOOKUP, 0) != 0);
         mEnablePeopleLookup.setChecked(Settings.System.getInt(cr,
-                Settings.System.ENABLE_PEOPLE_LOOKUP, 1) != 0);
+                Settings.System.ENABLE_PEOPLE_LOOKUP, 0) != 0);
         mEnableReverseLookup.setChecked(Settings.System.getInt(cr,
-                Settings.System.ENABLE_REVERSE_LOOKUP, 1) != 0);
+                Settings.System.ENABLE_REVERSE_LOOKUP, 0) != 0);
     }
 
     private void restoreLookupProviders() {